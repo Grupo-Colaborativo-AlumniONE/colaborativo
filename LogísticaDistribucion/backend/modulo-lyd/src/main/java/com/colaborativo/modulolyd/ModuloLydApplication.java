7d399522b6ad8b84cc7c2096cb9324621ef32094
Alejandro Escalante <alexalante2@gmail.com>
2023-08-30 21:28:10 -0600

    LogísticaDistribución_backend

---
+++ LogísticaDistribucion/backend/modulo-lyd/src/main/java/com/colaborativo/modulolyd/ModuloLydApplication.java
@@ -0,0 +1,13 @@
+package com.colaborativo.modulolyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloLydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloLydApplication.class, args);
+	}
+
+}
---
+++ LogísticaDistribucion/backend/modulo-lyd/src/test/java/com/colaborativo/modulolyd/ModuloLydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colaborativo.modulolyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloLydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
