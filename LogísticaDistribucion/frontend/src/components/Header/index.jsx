547a82de49eef1057d262ad7dfcd8664cbee0dd5
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-05 11:49:48 -0500

    add notificaction change when clicked hover in notification and avatar  responsive Logo
    se agrego cambio al clikear el icono de notificacion para posterior integrar anuncios importantes extraido de info del backend.
    
    hover en los iconos de notification y del avatar del usuario.
    
    en componente Logo cuando cambie el tamano de pantalla tambien cambie el tamano del nombre del area

--- LogísticaDistribucion/frontend/src/components/Header/index.jsx
+++ LogísticaDistribucion/frontend/src/components/Header/index.jsx
@@ -1,16 +1,12 @@
-/* import { useState } from 'react' */
+import { useState } from 'react'
 import Notifications from '@mui/icons-material/Notifications'
-/* import NotificationsActive from '@mui/icons-material/NotificationsActive' */
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
 import avatarimg from '../../assets/images/avatar.webp'
 import InputSearch from '../Inputs/InputSearch'
 import Logo from '../Logo'
 
 const Header = () => {
-/*   const [handleNotificationIcon, setHandleNotificationIcon] = useState(false)
-
-  const handleNotificationIconClick = () => {
-    setHandleNotificationIcon(!handleNotificationIcon)
-  } */
+  const [isCliked, setIsCliked] = useState(false)
 
   return (
       <header className="w-full h-20 pl-4 top-0 flex justify-between items-center fixed bg-backgroundSecondary backdrop-blur-xl">
@@ -22,17 +18,26 @@ const Header = () => {
         </div>
         <div className='rigth-header-container flex flex-row items-center gap-8 mr-4 '>
           <button
-           /*  onClick={handleNotificationIconClick} */
-            className='w-8 h-8 rounded-full ring-2 ring-gray-300 dark:ring-gray-500'
+            className='w-8 h-8 rounded-full ring-2 ring-gray-300 hover:ring-btnHover'
+            onClick={() => setIsCliked(!isCliked)}
           >
-            <Notifications
-              fontSize="medium"
-              sx={{ color: '#3898ec' }}
-            />
+            {isCliked
+              ? (
+              <NotificationsActiveIcon
+                fontSize="medium"
+                sx={{ color: '#d11009' }} // color pendiente de usar variable
+              />
+                )
+              : (
+              <Notifications
+                fontSize="medium"
+                sx={{ color: '#3898ec' }} // colorPrimary
+              />
+                )}
           </button>
 
           <img
-            className='w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500'
+            className='w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 hover:ring-btnHover'
             src={avatarimg} alt="avatar de usuario" />
         </div>
       </header>
