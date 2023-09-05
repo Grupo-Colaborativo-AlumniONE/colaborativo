de20ed087106c9fc10bd0d55d69e2d44cdf15328
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-05 13:00:39 -0500

    Update index.jsx

--- LogísticaDistribucion/frontend/src/pages/Home/index.jsx
+++ LogísticaDistribucion/frontend/src/pages/Home/index.jsx
@@ -1,12 +1,10 @@
 import Layout from '../../components/Layout'
-import AcUnitIcon from '@mui/icons-material/AcUnit'
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <AcUnitIcon fontSize="large" sx={{ color: '#3898ec' }} />
-        <h1 className='text-2xl text-colorSecondary'>
+        <h1 className='mt-5 text-2xl text-colorSecondary'>
           Hola este es el Home, busca en la carpeta pages/Home.jsx para que ahi pongas las secciones :)
           </h1>
       </Layout>
