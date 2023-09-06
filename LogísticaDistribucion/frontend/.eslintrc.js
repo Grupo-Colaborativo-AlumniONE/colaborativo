f89849480facd1d4a08cc7f0119f986d7ff30b46
Jefferson.art <canocs3@live.com>
2023-09-05 20:02:34 -0500

    add eslinttcr

--- LogísticaDistribucion/frontend/.eslintrc.js
+++ LogísticaDistribucion/frontend/.eslintrc.js
@@ -4,19 +4,13 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off'
