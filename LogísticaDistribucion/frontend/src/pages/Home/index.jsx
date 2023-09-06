8cd799502b388c03416d98481897fd40ea4f1e76
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-05 23:57:38 -0500

    Create Page TrackinProduct and table
    pagina TrackingProduct y su Tabla con informacion harcodeada para muestra

--- LogísticaDistribucion/frontend/src/pages/Home/index.jsx
+++ LogísticaDistribucion/frontend/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Layout from '../../components/Layout'
 
 const Home = () => {
@@ -7,6 +8,12 @@ const Home = () => {
         <h1 className='mt-5 text-2xl text-colorSecondary'>
           Hola este es el Home, busca en la carpeta pages/Home.jsx para que ahi pongas las secciones :)
           </h1>
+          {/* Este Link se posicionara en el sidebar cuando este listo */}
+          <Link to='/tracking-product'>
+            <button className='mt-5 bg-btnColor hover:bg-btnHover text-btnColorText font-bold py-2 px-4 rounded-md'>
+              Ir a Tracking Product
+            </button>
+          </Link>
       </Layout>
     </>
   )
