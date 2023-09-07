2fd3a95be5ebb501db6140d7f6ebf309fc3ce814
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-07 14:53:24 -0500

    add gradientLogo tailwind component

--- LogísticaDistribucion/frontend/src/components/Logo/index.jsx
+++ LogísticaDistribucion/frontend/src/components/Logo/index.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
 import Factory from '@mui/icons-material/Factory'
 
+/*       violet: 'from-[#FF1CF7] to-[#b249f8]',
+      yellow: 'from-[#FF705B] to-[#FFB457]',
+      blue: 'from-[#5EA2EF] to-[#0072F5]',
+      cyan: 'from-[#00b7fa] to-[#01cfea]',
+      green: 'from-[#6FEE8D] to-[#17c964]',
+      pink: 'from-[#FF72E1] to-[#F54C7A]',
+      foreground: 'dark:from-[#FFFFFF] dark:to-[#4B4B4B]' */
+
 const Logo = () => {
   return (
   <>
@@ -11,7 +19,9 @@ const Logo = () => {
         sx={{ color: '#3898ec' }}
       />
     </div>
-    <p className='capitalize font-sans leading-7 font-bold text-xs md:text-lg lg:text-2xl text-colorPrimary' >logistica y distribucion</p>
+    <p className='gradientLogo ' >
+      logistica y distribucion
+    </p>
   </Link>
   </>
   )
