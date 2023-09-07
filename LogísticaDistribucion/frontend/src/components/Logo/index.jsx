0e229277c2d4a1a51c1e7f3db5cf012e7a26185b
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-07 14:55:52 -0500

    move comment gradient

--- LogísticaDistribucion/frontend/src/components/Logo/index.jsx
+++ LogísticaDistribucion/frontend/src/components/Logo/index.jsx
@@ -1,14 +1,6 @@
 import { Link } from 'react-router-dom'
 import Factory from '@mui/icons-material/Factory'
 
-/*       violet: 'from-[#FF1CF7] to-[#b249f8]',
-      yellow: 'from-[#FF705B] to-[#FFB457]',
-      blue: 'from-[#5EA2EF] to-[#0072F5]',
-      cyan: 'from-[#00b7fa] to-[#01cfea]',
-      green: 'from-[#6FEE8D] to-[#17c964]',
-      pink: 'from-[#FF72E1] to-[#F54C7A]',
-      foreground: 'dark:from-[#FFFFFF] dark:to-[#4B4B4B]' */
-
 const Logo = () => {
   return (
   <>
@@ -19,7 +11,7 @@ const Logo = () => {
         sx={{ color: '#3898ec' }}
       />
     </div>
-    <p className='gradientLogo ' >
+    <p className='gradientLogo' >
       logistica y distribucion
     </p>
   </Link>
