742393cf0aabffe6bc72517c71404bc1ca62aec0
Jefferson.art <canocs3@live.com>
2023-09-12 17:44:03 -0500

    add margin top 160px

--- LogísticaDistribucion/frontend/src/components/Containers/ContainerMain.jsx
+++ LogísticaDistribucion/frontend/src/components/Containers/ContainerMain.jsx
@@ -1,6 +1,6 @@
 const ContainerMain = ({ titulo = 'Titulo asignado por defecto', children }) => {
   return (
-    <div className="container m-auto h-[50vh] bg-backgroundCard p-6 rounded-lg ">
+    <div className="container mt-40 m-auto h-[50vh] bg-backgroundCard p-6 rounded-lg ">
         <h1 className=" text-lg font-extrabold text-titleColor text-left">{titulo}</h1>
         {children}
     </div>
