ee175caf99a99fc2f8fb967a7ab7eff1e70abea5
Camilo Garcia <camilo12pq@gmail.com>
2023-09-19 16:26:51 -0500

    back gestion

---
+++ Gestion_proyecto_backend/src/test/java/com/example/Gestion_proyecto_backend/GestionProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_proyecto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProyectoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
